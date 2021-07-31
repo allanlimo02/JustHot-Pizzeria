d3c294d37b89f8963391374bbd0bb44c24c6da6f
allanlimo02 <allan.limo@student.moringaschool.com>
2021-07-31 18:45:32 +0300

    Fixed js unreachable error

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
+    var uName= $('#userName').val()
+
+
+    //form Validation
+    $('#checkdetails').click(function(){
+        if(uName==''){
+            $("#error1").show()
+            return false;
+        }
+    })
+
+    //Showing Delivery form
     $("#fordelivery").click(function(){
         $("#deldetails").show();
         $('.order-details').css('height','770px')
 
     })
+
     $("#forpickup").click(function(){
         $("#deldetails").hide();
         $('.order-details').css('height','440px')
         
     })
+    //Showing confirm order button
     $('#checkdetails').click(function(){
-        $("#confirm-order").show()    
+        $("#confirm-order").show()
+        $('#forName').append('Dear '+uName.val+' You ordered')    
     })
+    //show submit button
     $("#confirm-order").click(function(){
+        
+        
         $('#submit-btn').show()
         $('#reset-order').show()
 
